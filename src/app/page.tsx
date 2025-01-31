7fae8f2246a7962fcf109f68157b2670e90a3131
HARITJOSHI1 <joshiharit3@gmail.com>
2025-02-01 02:42:34 +0530

    init change and clerk user creation setup with webhook

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Button variant="ghost">Press me</Button>
+    </div>
+  );
+};
+
+export default page;
